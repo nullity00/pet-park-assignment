5e962eae4db416cc742f0bd009ba47eb3d34a758
nullity00 <reshmashaik3011@gmail.com>
2023-07-30 23:02:05 +0530

    test file done

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -5,10 +5,17 @@ contract PetPark {
 
   address public owner;
 
-  enum AnimalType { Dog, Cat, Rabbit, Fish, Parrot }
-  enum GenderType {
+  enum AnimalType { None, Dog, Cat, Rabbit, Fish, Parrot }
+  enum Gender {
     Male,
     Female
+  }
+
+  struct BorrowDetails {
+    bool borrowed;
+    AnimalType animalType;
+    uint age;
+    Gender gender;
   }
 
   event Added(AnimalType animalType, uint count);
@@ -20,44 +27,62 @@ contract PetPark {
     _;
   }
 
-  mapping (AnimalType => uint) public animalCount;
-  mapping (address => mapping (AnimalType => uint)) public detailedBorrowCount;
-  mapping (address => uint) public borrowCount;
+  mapping (AnimalType => uint) public animalCounts;
+  mapping (address => BorrowDetails) public borrowed;
+  mapping (AnimalType => uint) public borrowCounts;
 
   constructor() {
     owner = msg.sender;
   }
 
   function add(AnimalType animalType, uint count) public onlyOwner {
-    animalCount[animalType] += count;
+    require(animalType != AnimalType.None , "Invalid animal");
+    animalCounts[animalType] += count;
     emit Added(animalType, count);
   }
 
-  function borrow(uint age, GenderType gender, AnimalType animalType) public {
+  function borrow(uint _age, Gender _gender, AnimalType animalType) public {
 
-    require(animalCount[animalType] > 0, "No more animals of this type available");
-    require(borrowCount[msg.sender] < 2, "You can only borrow 1 animal");
+    require(animalType != AnimalType.None , "Invalid animal type");
+
+    if (borrowed[msg.sender].borrowed) {
+        if(borrowed[msg.sender].age == _age && borrowed[msg.sender].gender == _gender){
+          revert("Already adopted a pet");
+        }
+        if (borrowed[msg.sender].age != _age){
+          revert("Invalid Age");
+        }
+        if (borrowed[msg.sender].gender != _gender){
+          revert("Invalid Gender");
+        }
+      revert("Already adopted a pet");
+    }
+
+    require(_age > 0, "Age must be greater than 0");
+    require(animalCounts[animalType] > 0, "Selected animal not available");
     
-    if (gender == GenderType.Male) {
-      require(animalType == AnimalType.Dog || animalType == AnimalType.Fish, "Men can only borrow Dog or Fish");
+    if (_gender == Gender.Male) {
+      require(animalType == AnimalType.Dog || animalType == AnimalType.Fish, "Invalid animal for men");
     } else {
       if (animalType == AnimalType.Cat){
-        require(age >= uint(40), "Women under 40 cannot borrow Cat");
+        require(_age >= uint(40), "Invalid animal for women under 40");
       }
     }
 
-    detailedBorrowCount[msg.sender][animalType] += 1;
-    borrowCount[msg.sender] += 1;
+    borrowed[msg.sender] = BorrowDetails(true, animalType, _age, _gender);
+    animalCounts[animalType] -= 1; 
+    borrowCounts[animalType] += 1; 
     emit Borrowed(animalType);
 
   }
 
-  function giveBackAnimal(AnimalType animalType) public {
+  function giveBackAnimal() public {
 
-    require(detailedBorrowCount[msg.sender][animalType] > 0, "You have no animals to give back");
-    detailedBorrowCount[msg.sender][animalType] -= 1;
-    borrowCount[msg.sender] -= 1;
-    emit Returned(animalType);
+    require(borrowed[msg.sender].borrowed, "No borrowed pets");
+    borrowed[msg.sender].borrowed = false;
+    animalCounts[borrowed[msg.sender].animalType] += 1; 
+    borrowCounts[borrowed[msg.sender].animalType] -= 1;
+    emit Returned(borrowed[msg.sender].animalType);
 
   }
     
--- test/PetPark.t.sol
+++ test/PetPark.t.sol
@@ -26,8 +26,9 @@ contract PetParkTest is Test, PetPark {
     }
 
     function testCannotAddAnimalWhenNonOwner() public {
-        // 1. Complete this test and remove the assert line below
-        assert(false);
+        vm.prank(testPrimaryAccount);
+        vm.expectRevert("Only the owner can call this function");
+        petPark.add(AnimalType.Fish, 5);
     }
 
     function testCannotAddInvalidAnimal() public {
@@ -43,8 +44,8 @@ contract PetParkTest is Test, PetPark {
     }
 
     function testCannotBorrowWhenAgeZero() public {
-        // 2. Complete this test and remove the assert line below
-        assert(false);
+        vm.expectRevert("Age must be greater than 0");
+        petPark.borrow(0, Gender.Male, AnimalType.Fish);
     }
 
     function testCannotBorrowUnavailableAnimal() public {
@@ -127,8 +128,15 @@ contract PetParkTest is Test, PetPark {
     }
 
     function testBorrowCountDecrement() public {
-        // 3. Complete this test and remove the assert line below
-        assert(false);
+        petPark.add(AnimalType.Fish, 5);
+
+        petPark.borrow(24, Gender.Male, AnimalType.Fish);
+        uint incrementedBorrowCount = petPark.borrowCounts(AnimalType.Fish);
+
+        petPark.giveBackAnimal();
+        uint currentBorrowCount = petPark.borrowCounts(AnimalType.Fish);
+
+        assertEq(incrementedBorrowCount, currentBorrowCount + 1);
     }
 
     function testCannotGiveBack() public {
